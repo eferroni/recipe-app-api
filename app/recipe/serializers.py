f159f2de70ed80f4431853369b6b820c276b601a
Eduardo Ferroni <edu_ferroni@hotmail.com>
2021-10-18 22:24:41 -0300

    add create recipe features

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -32,3 +32,10 @@ class RecipeSerializer(serializers.ModelSerializer):
         model = Recipe
         fields = ("id", "title", "ingredients", "tags", "time_minutes", "price", "link")
         ready_only_fields = ("id",)
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for the recipe deyails object"""
+
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
